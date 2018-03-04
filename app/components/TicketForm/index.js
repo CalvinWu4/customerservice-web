7cd80a989ba8f7e44e9a5eb3f2d290709cd79d33
grahamhome <grahamhome333@gmail.com>
2018-03-04 16:44:23 -0500

    Added logic to fetch & store tickets from backend

--- app/components/TicketForm/index.js
+++ app/components/TicketForm/index.js
@@ -9,7 +9,6 @@ import React from 'react';
 
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
-import CenterGrid from 'components/CenterGrid';
 import Card, { CardContent } from 'material-ui/Card';
 import Icon from 'material-ui/Icon';
 
@@ -37,33 +36,31 @@ class TicketForm extends React.Component { // eslint-disable-line react/prefer-s
   }
   render() {
     return (
-      <CenterGrid>
-        <form>
-          <Card>
-            <CardContent>
-              <Button variant="fab" color="primary" label="edit" onClick={this.editForm} disabled={!(this.state.disabled) ? 'disabled' : ''}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" /> </svg>
-              </Button>
-              <h1>Ticket</h1>
-              <h3>Customer Details</h3>
-              <TextField label="Customer" name="CustomerName" defaultValue="First Last" disabled />
-              <TextField label="Customer Email" name="CustomerEmail" type="email" defaultValue="email@gmail.com" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
-              <h3>Issue Details</h3>
-              <TextField label="Issue" name="IssueDescription" defaultValue="Insert description of the issue with the KennUWare Device here" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
-              <TextField label="Agent Assigned" name="AgentAssigned" defaultValue="Best CS Agent" disabled />
-              <TextField label="Priority" name="Priority" defaultValue="High/Medium/Low" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
-              <TextField label="Complexity" name="Complexity" defaultValue="Complexity" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
-              <TextField id="date" name="OpenDate" label="Opened On" type="date" defaultValue="2018-02-24" disabled />
-              <h3>Product</h3>
-              <TextField id="product" name="Product" label="Product" defaultValue="KennUWare Device 2.0" disabled />
-              <TextField id="productSerialNum" name="ProductSerialNum" label="Product Serial Number" defaultValue="123820952323" type="number" disabled />
-              <Button variant="raised" color="secondary" label="save" onClick={this.editForm} disabled={(this.state.disabled) ? 'disabled' : ''}>
-                <Icon>Save</Icon>
-              </Button>
-            </CardContent>
-          </Card>
-        </form>
-      </CenterGrid>
+      <form>
+        <Card>
+          <CardContent>
+            <Button variant="fab" color="primary" label="edit" onClick={this.editForm} disabled={!(this.state.disabled) ? 'disabled' : ''}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" /> </svg>
+            </Button>
+            <h1>Ticket</h1>
+            <h3>Customer Details</h3>
+            <TextField label="Customer" name="CustomerName" defaultValue="First Last" disabled />
+            <TextField label="Customer Email" name="CustomerEmail" type="email" defaultValue="email@gmail.com" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
+            <h3>Issue Details</h3>
+            <TextField label="Issue" name="IssueDescription" defaultValue="Insert description of the issue with the KennUWare Device here" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
+            <TextField label="Agent Assigned" name="AgentAssigned" defaultValue="Best CS Agent" disabled />
+            <TextField label="Priority" name="Priority" defaultValue="High/Medium/Low" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
+            <TextField label="Complexity" name="Complexity" defaultValue="Complexity" disabled={(this.state.disabled) ? 'disabled' : ''} onChange={this.handleChange} />
+            <TextField id="date" name="OpenDate" label="Opened On" type="date" defaultValue="2018-02-24" disabled />
+            <h3>Product</h3>
+            <TextField id="product" name="Product" label="Product" defaultValue="KennUWare Device 2.0" disabled />
+            <TextField id="productSerialNum" name="ProductSerialNum" label="Product Serial Number" defaultValue="123820952323" type="number" disabled />
+            <Button variant="raised" color="secondary" label="save" onClick={this.editForm} disabled={(this.state.disabled) ? 'disabled' : ''}>
+              <Icon>Save</Icon>
+            </Button>
+          </CardContent>
+        </Card>
+      </form>
     );
   }
 }
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,20 +15,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import TicketPage from 'containers/TicketPage/Loadable';
-import HomePage from 'containers/HomePage/Loadable';
 import LoginPage from 'containers/LoginPage';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import { CustomerValidationPage } from '../CustomerValidationPage';
+import CustomerValidationPage from '../CustomerValidationPage/Loadable';
+import AgentDashboard from '../AgentDashboard/Loadable';
 
 export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={CustomerValidationPage} />
         <Route exact path="/agentLogin" component={LoginPage} />
-        <Route exact path="/customerLogin" component={CustomerValidationPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/ticket" component={TicketPage} />
+        <Route exact path="/tickets" component={AgentDashboard} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
