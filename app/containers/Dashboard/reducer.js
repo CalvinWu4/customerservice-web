4f544d070d527c1fe1a437e07457ca76c4cb6b7d
grahamhome <grahamhome333@gmail.com>
2018-03-25 12:54:51 -0400

    Added sample data

--- app/containers/Dashboard/reducer.js
+++ app/containers/Dashboard/reducer.js
@@ -8,7 +8,7 @@ import { fromJS } from 'immutable';
 import { GET_TICKETS_SUCCEDED } from './constants';
 
 const initialState = fromJS({
-  tickets: [],
+  tickets: [{title:'Device On Fire', model:'Fireproof Watch', openDate:'3/2/18', status:'Hot'}],
 });
 
 function dashboardReducer(state = initialState, action) {
