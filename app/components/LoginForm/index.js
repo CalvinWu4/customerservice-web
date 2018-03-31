4732e8301d3dac695b4f653756f88d369a033ad1
grahamhome <grahamhome333@gmail.com>
2018-03-31 17:20:43 -0400

    Added link to account creation form

--- app/components/LoginForm/index.js
+++ app/components/LoginForm/index.js
@@ -38,6 +38,8 @@ class LoginForm extends React.Component { // eslint-disable-line react/prefer-st
 
     this.validateEmail = this.validateEmail.bind(this);
     this.validatePassword = this.validatePassword.bind(this);
+
+    this.shouldRenderButton = this.shouldRenderButton.bind(this);
   }
 
   onChange(e) {
@@ -90,11 +92,19 @@ class LoginForm extends React.Component { // eslint-disable-line react/prefer-st
     return !isInvalid;
   }
 
+  shouldRenderButton() {
+    if (this.props.name === 'clients') {
+      return (<Button color="primary" onClick={() => this.props.redirectToCreateAccount()}>{"I don't have an account"}</Button>);
+    }
+    return null;
+  }
+
   render() {
     return (
       <Paper style={style.paper} elevation={4}>
         <Typography variant="headline" component="h3">Sign in</Typography>
         <Typography component="p">With your {this.props.name} account</Typography>
+        { this.shouldRenderButton() }
         <TextField style={style.emailTextField} label="Email" type="email" name="email" helperText={this.state.email.helperText} error={this.state.email.isInvalid} onChange={this.onChange} autoFocus />
         <TextField style={style.passwordTextField} label="Password" type="password" name="password" helperText={this.state.password.helperText} error={this.state.password.isInvalid} onChange={this.onChange} />
         <Typography style={style.forgotEmailLabel} component="p">Forgot email?</Typography>
@@ -107,6 +117,7 @@ class LoginForm extends React.Component { // eslint-disable-line react/prefer-st
 LoginForm.propTypes = {
   name: PropTypes.string.isRequired,
   onLogin: PropTypes.func.isRequired,
+  redirectToCreateAccount: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
