9ece695b610db113da5a58ad492f272dfa922ca2
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-04-20 02:32:55 -0400

    fixed: subtitle position in homepage

--- app/components/HomepageHeading/index.js
+++ app/components/HomepageHeading/index.js
@@ -17,8 +17,8 @@ class HomepageHeading extends React.Component { // eslint-disable-line react/pre
   render() {
     return (
       <Container text>
-        <Header as='h1' content='KennUWare' inverted style={{ fontFamily: 'aileronbold_italic', fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }} />
-        <Header as='h2' content='Top watches at a quality price.' inverted style={{ fontSize: '1.3em', fontWeight: 'normal', marginTop: '0.5em' }} />
+        <Header as='h1' content='KennUWare' inverted style={{ fontFamily: 'aileronbold_italic', fontSize: '4em', fontWeight: 'normal', paddingBottom: '0px', marginBottom: '0em', marginTop: '3em' }} />
+        <Header as='p' content='Mobile computing made easy. Seriously.' inverted style={{ fontSize: '1.25em', fontWeight: 'normal', marginTop: '0em', marginBottom: '2.5em' }} />
         <Button color='orange' style={{ textShadow: '1px 1px 0 #8e4110' }} size='huge'>
           Need help?
           <Icon name='right arrow' />
